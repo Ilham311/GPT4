dab10edd10ad003ac1cbadbe06364ea4a59540f0
Ilham311 <ilham@temp-mails.eu.org>
2024-12-24 03:28:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -55,4 +55,4 @@ def message():
         return jsonify({'response_type': 'success', 'response': response})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
